77cd254b6c5827dda2b264f6162c43be018f367c
Jeff Muizelaar <jrmuizel@gmail.com>
2019-03-18 17:08:14 -0400

    Order amd

---
+++ order-amd.py
@@ -0,0 +1,36 @@
+import json
+nv = open("amd.json").read()
+from collections import OrderedDict
+nv = json.loads(nv, object_pairs_hook=OrderedDict)
+devices = []
+for ven in nv.iteritems():
+    gen_devices = set()
+    for gen in ven[1].iteritems():
+        for chip in gen[1].iteritems():
+            for dev in chip[1]:
+                idev = int(dev, base=16)
+                devices.append([idev, gen[0], chip[0]])
+for d in sorted(devices):
+    up = d[0] >> 8
+    good = False
+    if d[0] >= 0x6600 and d[0] < 0x66b0:
+        good = True
+    if d[0] >= 0x6700 and d[0] < 0x6720:
+        good = True
+    if d[0] >= 0x6780 and d[0] < 0x6840:
+        good = True
+    if d[0] >= 0x6860 and d[0] < 0x6880:
+        good = True
+    if d[0] >= 0x6900 and d[0] < 0x6a00:
+        good = True
+    if d[0] == 0x7300:
+        good = True
+    if d[0] == 0x684c:
+        good = True
+    if d[0] >= 0x9830 and d[0] < 0x9870:
+        good = True
+    if d[0] >= 0x9900 and d[0] < 0x9a00:
+        good = True
+
+    if not good:
+        print hex(d[0]), d[1], d[2]
